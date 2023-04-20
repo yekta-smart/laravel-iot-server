6f392258caeea24a123f567ba98d11c20e2757ea
Amir H. Yeganemehr <yeganemehr@yahoo.com>
2023-04-20 16:16:26 +0330

    add new log type for changing product event

--- src/Contracts/LogType.php
+++ src/Contracts/LogType.php
@@ -4,6 +4,7 @@ namespace YektaSmart\IotServer\Contracts;
 
 enum LogType: string
 {
+    case ProductChanged = 'yekta-smart.iot.device.product-changed';
     case FirmwareChanged = 'yekta-smart.iot.device.firmware-changed';
     case HardwareChanged = 'yekta-smart.iot.device.hardware-changed';
 }
